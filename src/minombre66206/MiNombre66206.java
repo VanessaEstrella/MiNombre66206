ea25a4fbd4c684336e9b4c0ce1f75c6a18e7c69a
starco <starco@192.168.10.217>
2021-02-25 20:38:01 -0600

    es mi primer commit

---
+++ src/minombre66206/MiNombre66206.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package minombre66206;
+
+/**
+ *
+ * @author starco
+ */
+public class MiNombre66206 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String minombre = "Vanessa de los Angeles Estrella Uc";
+        System.out.println("mi nombre es:  " + minombre);
+    }
+    
+}
